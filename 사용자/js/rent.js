1a93536f831f9d2668159322184a5355edacfec7
reyeon1209 <reyeon5368@naver.com>
2020-06-16 19:57:52 +0900

    Create Rent

---
+++ 사용자/js/rent.js
@@ -0,0 +1,22 @@
+var stTime = new Date().getTime()               // 현재시간 timestamp를 stTime에 저장
+var timerStart = setInterval(function() {
+    var nowTime = new Date().getTime()          // 1ms당 한 번씩 현재시간 timestamp를 불러와서 nowTime에 저장
+    var newTime = new Date(nowTime - stTime)    // (nowTime - stTime)을 new Date()에
+    var hour = newTime.getHours() - 9           // 9??
+    var min = newTime.getMinutes()
+    var sec = newTime.getSeconds()
+
+    document.getElementById('post_hour').innerText = addZero(hour)
+    document.getElementById('post_min').innerText = addZero(min)
+    document.getElementById('post_sec').innerText = addZero(sec)
+}, 1)
+
+document.getElementById('end_rent').addEventListener('click', function() {
+    if (timerStart) {
+        clearInterval(timerStart)
+    }
+})
+
+function addZero(num) {
+    return (num < 10 ? '0'+num : ''+num)
+}
